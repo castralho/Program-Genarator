5bff3fdd7765db8e711545e9fc5888d9b6c475fa
Joaquim Castro <joaquimjacastro@gmail.com>
2024-10-17 13:25:25 +0100

    ordenação de tabela

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,63 +1,83 @@
-import React, { useState, useEffect } from 'react';
-import { SafeAreaView, View, Text, TouchableOpacity, StyleSheet, TextInput, StatusBar } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+  StatusBar,
+} from "react-native";
 
-const HomeScreen = ({navigation}) => {
-  
-    return (
-      <SafeAreaView style={styles.safeArea}>
-        {/* Botões */}
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={() => { /* Função para gerar músicas */ }}>
-            <Text style={styles.buttonText}>Gerar Plano</Text>
-          </TouchableOpacity>
-  
-          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('AddSong')}>
-            <Text style={styles.buttonText}>Adicionar Música</Text>
-          </TouchableOpacity>
-  
-          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('SongList')}>
-            <Text style={styles.buttonText}>Lista de Músicas</Text>
-          </TouchableOpacity>
-        </View>
-  
-        {/* Texto no fundo do ecrã */}
-        <Text style={styles.footerText}>Program Generator by @maneca and @castralho</Text>
-        <StatusBar style="auto" />
-      </SafeAreaView>
-    );
-  };
+const HomeScreen = ({ navigation }) => {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      {/* Botões */}
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            /* Função para gerar músicas */
+          }}
+        >
+          <Text style={styles.buttonText}>Gerar Plano</Text>
+        </TouchableOpacity>
 
-  const styles = StyleSheet.create({
-    safeArea: {
-      flex: 1,
-      justifyContent: 'flex-end',  //Posiciona o conteúdo no final do ecrã
-      alignItems: 'center',
-      padding: 20,
-      //backgroundColor: '#181B1E',
-    },
-    footerText: {
-      fontSize: 12,
-      color: '#888',  // Cor mais suave para o texto
-      marginBottom: 5,  // Pequena margem no fundo
-    },
-    buttonContainer: {
-      flex: 1,
-      justifyContent: 'center',
-      width: '95%',
-    },
-    button: {
-      backgroundColor: '#cf5d25',  // Cor de fundo do botão
-      paddingVertical: 30,  // Altura do botão
-      paddingHorizontal: 25,  // Largura interna
-      borderRadius: 15,  // Canto arredondado
-      marginBottom: 40,  // Espaço entre botões
-      alignItems: 'center',  // Centraliza o texto
-    },
-    buttonText: {
-      color: '#FFFFFF',  // Cor do texto
-      fontSize: 18,  // Tamanho do texto
-      fontWeight: '600',  // Peso da fonte
-    },
-  });  
-  
-  export default HomeScreen;
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("AddSong")}
+        >
+          <Text style={styles.buttonText}>Adicionar Música</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("SongList")}
+        >
+          <Text style={styles.buttonText}>Lista de Músicas</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Texto no fundo do ecrã */}
+      <Text style={styles.footerText}>Program Generator by @castralho</Text>
+      <Text style={styles.footerText}>Thought by @maneca</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    justifyContent: "flex-end", //Posiciona o conteúdo no final do ecrã
+    alignItems: "center",
+    padding: 20,
+    //backgroundColor: '#181B1E',
+  },
+  footerText: {
+    fontSize: 12,
+    color: "#888", // Cor mais suave para o texto
+    marginBottom: 5, // Pequena margem no fundo
+    textAlign: "center",
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "center",
+    width: "95%",
+  },
+  button: {
+    backgroundColor: "#cf5d25", // Cor de fundo do botão
+    paddingVertical: 30, // Altura do botão
+    paddingHorizontal: 25, // Largura interna
+    borderRadius: 15, // Canto arredondado
+    marginBottom: 40, // Espaço entre botões
+    alignItems: "center", // Centraliza o texto
+  },
+  buttonText: {
+    color: "#FFFFFF", // Cor do texto
+    fontSize: 18, // Tamanho do texto
+    fontWeight: "600", // Peso da fonte
+  },
+});
+
+export default HomeScreen;
--- screens/SongListScreen.js
+++ screens/SongListScreen.js
@@ -1,61 +1,171 @@
-import React, { useEffect, useState } from 'react';
-import { SafeAreaView, View, Text, FlatList, StyleSheet, StatusBar } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect, useState } from "react";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const SongListScreen = () => {
-    const [songs, setSongs] = useState([]);
-  
-    useEffect(() => {
-      const loadSongs = async () => {
-        const storedSongs = await AsyncStorage.getItem('songs');
-        if (storedSongs) {
-          setSongs(JSON.parse(storedSongs));
-        }
-      };
+  const [songs, setSongs] = useState([]);
+  const [sortOrder, setSortOrder] = useState({ field: null, direction: "asc" }); // Adiciona estado para a ordem de classificação
 
-      loadSongs();
+  useEffect(() => {
+    const loadSongs = async () => {
+      try {
+        const storedSongs = await AsyncStorage.getItem("songs");
+        if (storedSongs) {
+          // Parsea as músicas do armazenamento
+          const parsedSongs = JSON.parse(storedSongs);
+
+          // Ordena as músicas por número ao carregar
+          const sortedSongs = parsedSongs.sort(
+            (a, b) => Number(a.number) - Number(b.number)
+          );
+          setSongs(sortedSongs);
+        }
+      } catch (error) {
+        console.error(
+          "Já te fodeste ahah vais ter de procurar à moda antiga!",
+          error
+        );
+      }
+    };
+
+    loadSongs();
   }, []);
 
+  const handleSort = (field) => {
+    // Alterna entre as direções de classificação
+    const newDirection =
+      sortOrder.field === field && sortOrder.direction === "asc"
+        ? "desc"
+        : "asc";
+
+    // Atualiza a ordem de classificação
+    setSortOrder({ field, direction: newDirection });
+
+    // Ordena as músicas
+    const sortedSongs = [...songs].sort((a, b) => {
+      let comparison = 0;
+
+      if (field === "number") {
+        // Para números, converte-os para número e compara
+        comparison = Number(a[field]) - Number(b[field]);
+      } else {
+        // Para string, usa localeCompare
+        comparison = a[field].localeCompare(b[field]);
+      }
+
+      return newDirection === "asc" ? comparison : -comparison;
+    });
+    setSongs(sortedSongs);
+  };
+
   const renderSong = ({ item }) => (
-    <View style={styles.songItem}>
-      <Text style={styles.songText}>Número: {item.number}</Text>
-      <Text style={styles.songText}>Nome: {item.name}</Text>
-      <Text style={styles.songText}>Momento: {item.moment}</Text>
+    <View style={styles.row}>
+      <Text style={styles.columnNumber}>{item.number}</Text>
+      <Text style={styles.columnName}>{item.name}</Text>
+      <Text style={styles.columnMoment}>{item.moment}</Text>
     </View>
   );
 
   return (
     <SafeAreaView style={styles.safeArea}>
-      <StatusBar barStyle="dark-content" backgroundColor={styles.safeArea.backgroundColor} translucent={false} />
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor={styles.safeArea.backgroundColor}
+        translucent={false}
+      />
 
+      {/* Cabeçalhos da tabela, fixo no topo */}
+      <View style={styles.headerRow}>
+        <TouchableOpacity
+          onPress={() => handleSort("number")}
+          style={styles.headerColumn}
+        >
+          <Text style={styles.textButton}>Número</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => handleSort("name")}
+          style={styles.headerColumn}
+        >
+          <Text style={styles.textButton}>Nome</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => handleSort("moment")}
+          style={styles.headerColumn}
+        >
+          <Text style={styles.textButton}>Momento</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Lista de músicas */}
       <FlatList
         data={songs}
+        keyExtractor={(item, index) => index.toString()} // Usa o índice como key temporária
         renderItem={renderSong}
-        keyExtractor={(item) => item.number.toString()}
-        style={styles.songList}
+        contentContainerStyle={styles.listContent}
+        style={styles.list} // Adiciona estilo para ocupar o restante da tela
       />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-    safeArea: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#f5f5f5',
-    },
-    songList: {
-      width: '100%',
-    },
-    songItem: {
-      padding: 10,
-      borderBottomWidth: 1,
-      borderBottomColor: '#ccc',
-    },
-    songText: {
-      fontSize: 16,
-    },
-  });
-  
-  export default SongListScreen;
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  headerRow: {
+    flexDirection: "row",
+    backgroundColor: "#ddd",
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  // Cabeçalhos da tabela com largura específica para cada coluna
+  headerColumn: {
+    flex: 1, // Permite que cada coluna ocupe espaço igual
+    alignItems: "left", // Centraliza o texto
+    justifyContent: "center", // Alinha o texto verticalmente
+    paddingHorizontal: 10,
+  },
+  textButton: {
+    fontWeight: "bold",
+  },
+  list: {
+    flex: 1, // Garante que a lista ocupe o restante da tela
+  },
+  listContent: {
+    flexGrow: 1, // Garante que o conteúdo cresça para preencher o espaço da lista
+  },
+  row: {
+    flexDirection: "row",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  columnNumber: {
+    flex: 1,
+    textAlign: "left",
+  },
+  columnName: {
+    flex: 2,
+    textAlign: "left",
+    paddingHorizontal: "5%", // Adiciona espaçamento à esquerda e à direita
+  },
+  columnMoment: {
+    flex: 2,
+    textAlign: "left",
+    paddingLeft: "5%", // Adiciona espaçamento à esquerda
+  },
+});
+
+export default SongListScreen;
